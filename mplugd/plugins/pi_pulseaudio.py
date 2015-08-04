2a555a29576da1e1e6ca0eb38356ea99694d0335
Ben Klopfenstein <bklopfenstein@vmware.com>
2015-08-04 01:24:38 -0600

    fix up ifs

--- mplugd/plugins/pi_pulseaudio.py
+++ mplugd/plugins/pi_pulseaudio.py
@@ -359,7 +359,7 @@ class Stream(PA_object):
 		if attr == "name" or attr == "Name":
 			if "application.name" in self._props:
 				return self._props["application.name"][:-1]
-			else if "device.description" in self._props:
+			elif "device.description" in self._props:
 				return self._props["device.description"][:-1]
 		return PA_object.__getattr__(self, attr)
 
