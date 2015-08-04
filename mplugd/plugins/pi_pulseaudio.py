5351d7f824f8287319a00525b1342651c500ba0a
Ben Klopfenstein <bklopfenstein@vmware.com>
2015-08-04 02:40:03 -0600

    use media.name if application.name is missing
    For bluetooth audio streams, media.name contains the name of the device streaming audio.

--- mplugd/plugins/pi_pulseaudio.py
+++ mplugd/plugins/pi_pulseaudio.py
@@ -362,6 +362,8 @@ class Stream(PA_object):
 		if attr == "name" or attr == "Name":
 			if "application.name" in self._props:
 				return self._props["application.name"][:-1]
+			elif "media.name" in self._props:
+				return self._props["media.name"][:-1]
 			elif "device.description" in self._props:
 				return self._props["device.description"][:-1]
 		return PA_object.__getattr__(self, attr)
